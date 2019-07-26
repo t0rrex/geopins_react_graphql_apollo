04f71f768847f1eba04dc7865b4316106c5c2dc3
Gleb Andreev <gandreev@elinext.com>
2019-07-26 14:48:10 +0300

    Hooks instead of redux, useContext, useReducer to save User

--- client/src/index.js
+++ client/src/index.js
@@ -13,13 +13,16 @@ import * as serviceWorker from "./serviceWorker";
 const Root = () => {
     const initialState = useContext(Context)
     const [state, dispatch] = useReducer(reducer, initialState)
+    console.log({ state })
 
     return (
         <Router>
+            <Context.Provider value={{ state, dispatch }}>
             <Switch>
                 <Route exact path="/" component={App}/>
                 <Route path="/login" component={Splash}/>
             </Switch>
+            </Context.Provider>
         </Router>
     );
 };
