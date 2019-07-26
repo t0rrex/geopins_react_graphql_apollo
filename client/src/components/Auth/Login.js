bd06aa60cebfaf05211b75d53f4968e6cc947643
Gleb Andreev <gandreev@elinext.com>
2019-07-26 13:49:36 +0300

    Google OAuth, server side configuring

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { GraphQLClient } from 'graphql-request'
+import {GoogleLogin} from 'react-google-login';
+import {withStyles} from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
-const Login = ({ classes }) => {
-  return <div>Login</div>;
+const ME_QUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+ }
+`;
+
+const Login = ({classes}) => {
+
+    const onSuccess = async googleUser => {
+        const idToken = googleUser.getAuthResponse().id_token
+        const client = new GraphQLClient('http://localhost:4000/graphql', {
+            headers: { authorization: idToken }
+        });
+
+        const data = await client.request(ME_QUERY);
+
+        console.log({ data })
+    }
+
+    return <GoogleLogin
+        clientId="241914070630-err0q0679hqsmvr7hck00nc4o48bmcfu.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        isSignedIn={ true }
+    />;
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+    root: {
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center"
+    }
 };
 
 export default withStyles(styles)(Login);
